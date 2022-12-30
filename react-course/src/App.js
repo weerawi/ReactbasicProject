b03949c7383fd681c5c26a4eda6be157c7b42564
weerawi <weerawikramatharindu@gmail.com>
2022-12-30 14:01:46 +0530

    scond

---
+++ react-course/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo.js";
+function App() {
+  return (
+    <div>
+      <h1>My Tools</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
+    </div>
+  );
+}
+
+export default App;
