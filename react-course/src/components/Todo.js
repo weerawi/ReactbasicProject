25f574ac44b4a5fe5ed070f6d514fd931c77dcce
weerawi <weerawikramatharindu@gmail.com>
2023-01-02 22:47:57 +0530

    second commit

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
+import Modal from "./Modal.js";
+import Backdrop from "./Backdrop.js";
+
 function Todo(props) {
+  // initial state of modal is false
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="action">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+        {/* Not use parantheseis () in after deletehandler bcz it evaluate before function btn click */}
+
+        {modalIsOpen && (
+          <Modal onCancel={closeModalHandler} onCancel={closeModalHandler} />
+        )}
+
+        {/* {modalIsOpen ? <Modal /> : null} */}
+
+        {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
       </div>
     </div>
   );
